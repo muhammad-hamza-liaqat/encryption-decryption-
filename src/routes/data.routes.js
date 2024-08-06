b2c4c3aca431a25d2988aa163dd87bce1888cf5e
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-08-06 15:48:15 +0500

    basic code setup

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -3,7 +3,5 @@ const { defaultFunction, encryptFunction, decryptFunction } = require("../contro
 const dataRoutes = express.Router();
 
 dataRoutes.get("/", defaultFunction);
-dataRoutes.post("/en", encryptFunction);
-dataRoutes.post("/de", decryptFunction);
 
 module.exports = dataRoutes
